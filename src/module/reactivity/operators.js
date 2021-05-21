5356ec0941a0925aa19da5542cbd200562779de5
Error4767 <2303184060@qq.com>
2021-05-21 19:04:44 +0800

    reactivity: optimize array change action trigger effect

--- src/module/reactivity/operators.js
+++ src/module/reactivity/operators.js
@@ -34,14 +34,22 @@ function createProxySetter(transform = v => v) {
     if (oldValue !== value) {
       const newValue = transform(value);
 
+      let length = Array.isArray(target) ? target.length : 0;
       // 是ref设置其value，不是ref直接设置其值
       const operationState = isRef(target[key]) ? Reflect.set(target[key], 'value', newValue) : Reflect.set(target, key, newValue);
 
       trigger(target, key, oldValue, newValue);
 
-      // 如果有依赖属性则是ownKeys操作器收集了依赖或者是数组中的东西，触发effect
-      target?.[operateEffects.effectsIdentify]?.forEach(dep => dep.notify());
-
+      // 如果有依赖属性
+      if(Array.isArray(target)) {
+        // 是数组,如果是新增元素就触发数组上的effect
+        if(String(key) === String(length)) {// 都转化为字符串比较,如果key=length就是数组新增操作
+          target?.[operateEffects.effectsIdentify]?.forEach(dep => dep.notify()); 
+        }
+      }else {
+        // 不是数组，则是ownKeys操作器收集了依赖，触发effect
+        target?.[operateEffects.effectsIdentify]?.forEach(dep => dep.notify());
+      }
       return operationState;
     }
     return true;
