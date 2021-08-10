c4cd766cd427a4d1f41ac842dcf807944732d255
Error4767 <2303184060@qq.com>
2021-08-10 22:22:16 +0800

    fix drag mobile page default event problem

--- src/module/drag.js
+++ src/module/drag.js
@@ -1,41 +1,42 @@
 export function drag(obj) {
-	let dragOffsetLeft, dragOffsetTop;
-	// PC 
-	let onMouseDownFunction = (e) => {
-		e = e || window.event;
-		document.addEventListener('mousemove', onMouseMoveFunction, false);
-		dragOffsetLeft = document.documentElement.scrollLeft + e.clientX - obj.offsetLeft;
-		dragOffsetTop = document.documentElement.scrollTop + e.clientY - obj.offsetTop;
-	}
-	let onMouseMoveFunction = (e) => {
-		e = e || window.event;
-		let elementLeft = document.documentElement.scrollLeft + e.clientX - dragOffsetLeft,
-			elementTop = document.documentElement.scrollTop + e.clientY - dragOffsetTop;
-		obj.style.cssText += ';left:' + elementLeft + 'px;top:' + elementTop + 'px;';
-	}
-	let onMouseUpFunction = (e) => {
-		e = e || window.event;
-		document.removeEventListener('touchstart', onMouseMoveFunction, false);
-	}
-	obj.addEventListener('mousedown', onMouseDownFunction, false);
-	obj.addEventListener('mouseup', onMouseUpFunction, false);
+  let dragOffsetLeft, dragOffsetTop;
+  // PC 
+  let onMouseDownFunction = (e) => {
+    e = e || window.event;
+    document.addEventListener('mousemove', onMouseMoveFunction, false);
+    dragOffsetLeft = document.documentElement.scrollLeft + e.clientX - obj.offsetLeft;
+    dragOffsetTop = document.documentElement.scrollTop + e.clientY - obj.offsetTop;
+  }
+  let onMouseMoveFunction = (e) => {
+    e = e || window.event;
+    let elementLeft = document.documentElement.scrollLeft + e.clientX - dragOffsetLeft,
+      elementTop = document.documentElement.scrollTop + e.clientY - dragOffsetTop;
+    obj.style.cssText += ';left:' + elementLeft + 'px;top:' + elementTop + 'px;';
+  }
+  let onMouseUpFunction = (e) => {
+    e = e || window.event;
+    document.removeEventListener('touchstart', onMouseMoveFunction, false);
+  }
+  obj.addEventListener('mousedown', onMouseDownFunction, false);
+  obj.addEventListener('mouseup', onMouseUpFunction, false);
 
-	// 移动端
-
-	let onTouchStartFunction = (e) => {
-		document.addEventListener('touchmove', onTouchMoveFunction);
-		dragOffsetLeft = document.documentElement.scrollLeft + e.changedTouches[0].clientX - obj.offsetLeft;
-		dragOffsetTop = document.documentElement.scrollTop + e.changedTouches[0].clientY - obj.offsetTop;
-	}
-	let onTouchMoveFunction = (e) => {
-		let elementLeft = document.documentElement.scrollLeft + e.changedTouches[0].clientX - dragOffsetLeft,
-			elementTop = document.documentElement.scrollTop + e.changedTouches[0].clientY - dragOffsetTop;
-		obj.style.cssText += ';left:' + elementLeft + 'px;top:' + elementTop + 'px;';
-	}
-	let onTouchEndFunction = (e) => {
-		document.removeEventListener('touchmove', onTouchMoveFunction);
-	}
-
-	obj.addEventListener("touchstart", onTouchStartFunction);
-	obj.addEventListener("touchend", onTouchEndFunction);
+  // 移动端
+  let onTouchStartFunction = (e) => {
+    document.addEventListener('touchmove', onTouchMoveFunction, { passive: false } /* 阻止浏览器默认的下拉刷新 */);
+    dragOffsetLeft = document.documentElement.scrollLeft + e.changedTouches[0].clientX - obj.offsetLeft;
+    dragOffsetTop = document.documentElement.scrollTop + e.changedTouches[0].clientY - obj.offsetTop;
+  }
+  let onTouchMoveFunction = (e) => {
+    // 阻止页面的默认滚动
+    e.preventDefault();
+    let elementLeft = document.documentElement.scrollLeft + e.changedTouches[0].clientX - dragOffsetLeft,
+      elementTop = document.documentElement.scrollTop + e.changedTouches[0].clientY - dragOffsetTop;
+    obj.style.cssText += ';left:' + elementLeft + 'px;top:' + elementTop + 'px;';
+  }
+  let onTouchEndFunction = (e) => {
+    e.preventDefault();
+    document.removeEventListener('touchmove', onTouchMoveFunction);
+  }
+  obj.addEventListener("touchstart", onTouchStartFunction);
+  obj.addEventListener("touchend", onTouchEndFunction);
 }
