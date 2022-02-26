c1aff5994613c69c09a674637c6b314bc7985c0d
Error4767 <2303184060@qq.com>
2022-02-26 13:20:28 +0800

    reactivity: fix shallow reactive set a object attribute is a reactiveObject's problem

--- src/module/reactivity/reactive.js
+++ src/module/reactivity/reactive.js
@@ -25,17 +25,19 @@ function isReactive(obj) {
 }
 
 // 创建handlers的函数
-function createHandler(handlerType) { // handlerType: undefined / "Map" / "Set"
+function createHandler(
+  // handlerType: undefined / "Map" / "Set"
+  handlerType = undefined,
   // setter转换器，如果是一般对象，则转化为响应式对象
-  let setterTransformer = (v) => {// 新数据转换为响应式代理
+  setterTransformer = (v) => {// 新数据转换为响应式代理
     return reactive(v);
   }
-
+) {
   return {
     // 如果是 map, set 就使用对应 getter，否则使用默认getter
-    get: handlerType === "Map" 
-    ? createProxyMapGetter(setterTransformer) 
-    : (handlerType === "Set" ? createProxySetGetter(setterTransformer) : proxyGetter),
+    get: handlerType === "Map"
+      ? createProxyMapGetter(setterTransformer)
+      : (handlerType === "Set" ? createProxySetGetter(setterTransformer) : proxyGetter),
     set: createProxySetter(setterTransformer),
     ownKeys: proxyOwnKeysHandler,
     deleteProperty: proxyDeleteHandler,
@@ -48,6 +50,11 @@ const proxyDefaultHandlers = createHandler();
 const proxyMapHandlers = createHandler("Map");
 const proxySetHandlers = createHandler("Set");
 
+// 创建shallReactive handlers
+const proxyDefaultHandlersShallow = createHandler(undefined, (v) => v);
+const proxyMapHandlersShallow = createHandler("Map", (v) => v);
+const proxySetHandlersShallow = createHandler("Set", (v) => v);
+
 // 判断是否可以转化为响应式对象
 function isTransformableToReactive(obj) {
   // 特殊对象或者不是对象直接返回
@@ -58,11 +65,19 @@ function isTransformableToReactive(obj) {
 }
 
 // 创建反应式对象,只在该对象上，不递归创建（无条件判断，内部方法）
-function createReactiveObject(obj) {
+function createReactiveObject(obj, isShallow = false) {
   // 检测类型
   let objType = typeOf(obj);
-  // 根据类型选择对应handlers
-  let handler = objType === "Map" ? proxyMapHandlers : ( objType === "Set" ? proxySetHandlers : proxyDefaultHandlers);
+
+  let handler;
+  // 是否是浅层
+  if (isShallow) {
+    handler = objType === "Map" ? proxyMapHandlersShallow : (objType === "Set" ? proxySetHandlersShallow : proxyDefaultHandlersShallow);
+  } else {
+    // 根据类型选择对应handlers
+    handler = objType === "Map" ? proxyMapHandlers : (objType === "Set" ? proxySetHandlers : proxyDefaultHandlers);
+  }
+
   // 设置标识
   setIdentify(obj, REACTIVE_IDENTIFY);
   // 数组额外处理
@@ -75,7 +90,7 @@ function createReactiveObject(obj) {
 
 // 浅层反应式对象
 function shallowReactive(obj) {
-  return isTransformableToReactive(obj) ? createReactiveObject(obj) : obj;
+  return isTransformableToReactive(obj) ? createReactiveObject(obj, true) : obj;
 }
 
 function reactive(obj) {
